a311a80920e10d219d722f89ea74b1a78cceed83
Sergey Litvinov <slitvinov@gmail.com>
2023-11-23 12:10:59 -0500

    up

--- train.py
+++ train.py
@@ -6,53 +6,56 @@ import matplotlib.pyplot as plt
 
 
 def exact(t, x):
-    return np.sin(math.pi * x) * math.cos(2 * math.pi * t) + np.sin(
+    return math.sin(math.pi * x) * math.cos(2 * math.pi * t) + math.sin(
         4 * math.pi * x) * math.cos(8 * math.pi * t) / 2
 
-def _closure():
-    u_tt = (u[2:, 1:-1] - 2 * u[1:-1, 1:-1] + u[:-2, 1:-1]) * (nt - 1)**2
-    u_xx = (u[1:-1, 2:] - 2 * u[1:-1, 1:-1] + u[1:-1, :-2]) * (nx - 1)**2
-    pde = torch.mean((u_tt - 4 * u_xx)**2)
-    ic = torch.mean((u[0, :] - u_ic)**2)
-    bc = torch.mean(u[:, 0]**2 + u[:, -1]**2)
-    loss = pde + li * ic + lb * bc + 0.5 * (mi * ic**2 + mb * bc**2)
-    return loss, pde, ic, bc
-
-def closure():
-    opt.zero_grad()
-    loss, *rest = _closure()
-    loss.backward()
-    return loss
+class Closure:
+    def __call__(self):
+        opt.zero_grad()
+        u_tt = (u[2:, 1:-1] - 2 * u[1:-1, 1:-1] + u[:-2, 1:-1]) * (nt - 1)**2
+        u_xx = (u[1:-1, 2:] - 2 * u[1:-1, 1:-1] + u[1:-1, :-2]) * (nx - 1)**2
+        self.pde = torch.mean((u_tt - 4 * u_xx)**2)
+        self.ic = torch.mean((u[0, :] - u_ic)**2)
+        self.bc = torch.mean(u[:, 0]**2 + u[:, -1]**2)
+        loss = self.pde + li * self.ic + lb * self.bc + 0.5 * (mi * self.ic**2 + mb * self.bc**2)
+        loss.backward()
+        return loss
 
 torch.set_default_dtype(torch.float64)
 torch.manual_seed(0)
 np.random.seed(0)
-nx, nt = 15, 30
+nt, nx  = 30, 15
 b = math.sqrt(6 / (nt * nx))
 u = torch.zeros((nt, nx), requires_grad=True)
-opt = torch.optim.LBFGS([u], tolerance_grad=0, tolerance_change=0)
+opt = torch.optim.LBFGS([u], tolerance_grad=0, tolerance_change=0, history_size=15)
 li, lb = 0, 0
 mi, mb = 1, 1
 epsilon = 1e-16
 mu_max = 10000
 eta = 0
 x = np.linspace(0, 1, nx)
-u_ic = torch.tensor(exact(0, x))
-for epoch in range(101):
-    opt.step(closure)
-    loss, pde, ic, bc = _closure()
+u_ic = torch.tensor([exact(0, x) for x in x])
+c = Closure()
+for epoch in range(51):
+    opt.step(c)
     with torch.no_grad():
-        penalty = ic**2 + bc**2
+        penalty = c.ic**2 + c.bc**2
         if penalty >= eta/16 and penalty > epsilon:
             mi = min(2 * mi, mu_max)
             mb = min(2 * mb, mu_max)
-            li += mi * ic
-            lb += mb * bc
+            li += mi * c.ic
+            lb += mb * c.bc
         eta = penalty
-    if epoch % 20 == 0:
-        print(f": {epoch + 1:3d} {pde.detach().item():2.3e}")
+    if epoch % 10 == 0:
+        print(f": {epoch + 1:3d} {c.pde.detach().item():2.3e}")
 
+u = u.detach().numpy()
+t = np.linspace(0, 1, nt)
+u_e = [[exact(t, x) for x in x] for t in t]
+l2rel = np.linalg.norm(u - u_e) / np.linalg.norm(u_e)
+print("l2rel: %.6e" % l2rel)
 for ti in 0, nt//2, 3 * nt// 4, nt - 1:
-    plt.plot(x, u[ti, :].detach().numpy(), "ko", markerfacecolor="none")
-    plt.plot(x, exact(ti / (nt - 1), x), "k-")
+    time = ti / (nt - 1)
+    plt.plot(x, u[ti], "ko", markerfacecolor="none")
+    plt.plot(x, [exact(time, x) for x in x], "k-")
 plt.savefig("train.png")
