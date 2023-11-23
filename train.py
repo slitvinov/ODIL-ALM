3ec1b4d090326c9d2fef9b7839dca915d36c1fbd
Sergey Litvinov <slitvinov@gmail.com>
2023-11-23 12:34:03 -0500

    yapf -i

--- train.py
+++ train.py
@@ -9,7 +9,9 @@ def exact(t, x):
     return math.sin(math.pi * x) * math.cos(2 * math.pi * t) + math.sin(
         4 * math.pi * x) * math.cos(8 * math.pi * t) / 2
 
+
 class Closure:
+
     def __call__(self):
         opt.zero_grad()
         u_tt = (u[2:, 1:-1] - 2 * u[1:-1, 1:-1] + u[:-2, 1:-1]) * (nt - 1)**2
@@ -17,17 +19,22 @@ class Closure:
         self.pde = torch.mean((u_tt - 4 * u_xx)**2)
         self.ic = torch.mean((u[0, :] - u_ic)**2)
         self.bc = torch.mean(u[:, 0]**2 + u[:, -1]**2)
-        loss = self.pde + li * self.ic + lb * self.bc + 0.5 * (mi * self.ic**2 + mb * self.bc**2)
+        loss = self.pde + li * self.ic + lb * self.bc + 0.5 * (
+            mi * self.ic**2 + mb * self.bc**2)
         loss.backward()
         return loss
+
 
 torch.set_default_dtype(torch.float64)
 torch.manual_seed(0)
 np.random.seed(0)
-nt, nx  = 30, 15
+nt, nx = 30, 15
 b = math.sqrt(6 / (nt * nx))
 u = torch.zeros((nt, nx), requires_grad=True)
-opt = torch.optim.LBFGS([u], tolerance_grad=0, tolerance_change=0, history_size=15)
+opt = torch.optim.LBFGS([u],
+                        tolerance_grad=0,
+                        tolerance_change=0,
+                        history_size=15)
 li, lb = 0, 0
 mi, mb = 1, 1
 epsilon = 1e-16
@@ -40,7 +47,7 @@ for epoch in range(51):
     opt.step(c)
     with torch.no_grad():
         penalty = c.ic**2 + c.bc**2
-        if penalty >= eta/16 and penalty > epsilon:
+        if penalty >= eta / 16 and penalty > epsilon:
             mi = min(2 * mi, mu_max)
             mb = min(2 * mb, mu_max)
             li += mi * c.ic
@@ -52,9 +59,9 @@ for epoch in range(51):
 u = u.detach().numpy()
 t = np.linspace(0, 1, nt)
 u_e = [[exact(t, x) for x in x] for t in t]
-l2rel = np.linalg.norm(u - u_e) / np.linalg.norm(u_e)
-print("l2rel: %.6e" % l2rel)
-for ti in 0, nt//2, 3 * nt// 4, nt - 1:
+print("relative l2 error: %.6e" %
+      (np.linalg.norm(u - u_e) / np.linalg.norm(u_e)))
+for ti in 0, nt // 2, 3 * nt // 4, nt - 1:
     time = ti / (nt - 1)
     plt.plot(x, u[ti], "ko", markerfacecolor="none")
     plt.plot(x, [exact(time, x) for x in x], "k-")
